54e201e7c36d327049dfb624583d92011f3ca1b7
Sanketh G Hebbar <90565855+Sanketh-Hebbar@users.noreply.github.com>
2022-07-08 20:33:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Cloud \n'
+  const msg = 'Hello Varun \n'
   res.end(msg);
 });
 
