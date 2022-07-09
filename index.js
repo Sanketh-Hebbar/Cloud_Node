d2ad7c669fc701623d8fee8cda1d52bdb2ad6413
Sanketh-Hebbar <sankethebbar10@gmail.com>
2022-07-09 17:33:15 +0530

    first commit

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-//<<<<<<< HEAD
+
   const msg = 'Hello deployment was successfull  \n'
-//=======
- // const msg = 'Hello Varun \n'
-//>>>>>>> 54e201e7c36d327049dfb624583d92011f3ca1b7
+
   res.end(msg);
 });
 
